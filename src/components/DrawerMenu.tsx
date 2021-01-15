41639ae19b99f548bc7c43b0d0b0977f8395adf4
Keiya Watanabe <ohagi2149@gmail.com>
2021-01-16 00:48:13 +0900

    clean: css定義をコンポーネント内に移動

--- src/components/DrawerMenu.tsx
+++ src/components/DrawerMenu.tsx
@@ -11,8 +11,26 @@ import OpenInNewIcon from '@material-ui/icons/OpenInNew'
 import { AppContext } from '../contexts/AppContext'
 
 const useStyles = makeStyles({
-  list: { width: 250 },
+  list: { width: 250, height: '100%' },
   fullList: { width: 'auto' },
+  spaceOuter: {
+    height: 'calc(100% - 225px)',
+    maskImage:
+      'radial-gradient(#000 20%, transparent 20%), radial-gradient(#000 20%, transparent 20%)',
+    maskSize: '15px 15px',
+    maskPosition: '0 0, 7px 7px',
+  },
+  space: {
+    height: '100%',
+    maskImage: 'linear-gradient(0deg, #000 20%, transparent 100%)',
+  },
+  spaceInner: {
+    background:
+      'linear-gradient( 30deg, #f1c40f, #e74c3c, #1abc9c, #3498db, #9b59b6, #f1c40f)',
+    backgroundSize: '500% 500%',
+    animation: 'slideGradient 15s ease infinite',
+    height: '100%',
+  },
 })
 
 interface Props {
@@ -80,9 +98,9 @@ export default function DrawerMenu(props: Props) {
               <ListItemText primary={t('DRAWER.ABOUT_THIS_APP')} />
             </ListItem>
           </List>
-          <div className="drawer-outer">
-            <div className="drawer">
-              <div className="drawer-inner"></div>
+          <div className={classes.spaceOuter}>
+            <div className={classes.space}>
+              <div className={classes.spaceInner}></div>
             </div>
           </div>
         </div>
