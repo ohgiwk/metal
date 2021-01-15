5f35c764644240ed4d3d763976e81bc1b84d8b32
Keiya Watanabe <ohagi2149@gmail.com>
2021-01-16 00:48:47 +0900

    clean: コメント削除

--- src/components/GroupList.tsx
+++ src/components/GroupList.tsx
@@ -1,12 +1,18 @@
 import React, { useContext } from 'react'
 import { useTranslation } from 'react-i18next'
-import * as MUI from '@material-ui/core'
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListItemSecondaryAction,
+  ListSubheader,
+} from '@material-ui/core'
 import FolderIcon from '@material-ui/icons/Folder'
+import DeleteIcon from '@material-ui/icons/Delete'
 import FolderSpecialIcon from '@material-ui/icons/FolderSpecial'
 
 import OptionMenu from './OptionMenu'
-import DeleteIcon from '@material-ui/icons/Delete'
-
 import { ListContext } from '../contexts/ListContext'
 
 const GroupList: React.FC<{}> = () => {
@@ -20,43 +26,36 @@ const GroupList: React.FC<{}> = () => {
   } = useContext(ListContext)
 
   return (
-    <MUI.List
-      dense
-      subheader={<MUI.ListSubheader>{t('HOME.GROUPS')}</MUI.ListSubheader>}
-    >
-      <MUI.ListItem
+    <List dense subheader={<ListSubheader>{t('HOME.GROUPS')}</ListSubheader>}>
+      <ListItem
         button
         selected={!selectedGroup}
         onClick={() => {
           setSelectedGroup(undefined)
         }}
       >
-        <MUI.ListItemIcon>
+        <ListItemIcon>
           <FolderSpecialIcon />
-        </MUI.ListItemIcon>
-        <MUI.ListItemText primary="ALL" />
-      </MUI.ListItem>
+        </ListItemIcon>
+        <ListItemText primary="ALL" />
+      </ListItem>
       {groups.map((group, i) => (
         <React.Fragment key={i}>
-          <MUI.ListItem
+          <ListItem
             button
             selected={selectedGroup?.id === group.id}
             onClick={() => {
               setSelectedGroup(group)
               setSelectedEntry(undefined)
-
-              // const arr = [...groupOpens]
-              // arr[i] = !arr[i]
-              // setGroupOpens(arr)
             }}
           >
-            <MUI.ListItemIcon>
+            <ListItemIcon>
               <FolderIcon />
-            </MUI.ListItemIcon>
-            <MUI.ListItemText primary={group.name} />
+            </ListItemIcon>
+            <ListItemText primary={group.name} />
 
             {selectedGroup?.id === group.id && (
-              <MUI.ListItemSecondaryAction>
+              <ListItemSecondaryAction>
                 <OptionMenu
                   items={[
                     {
@@ -66,26 +65,12 @@ const GroupList: React.FC<{}> = () => {
                     },
                   ]}
                 />
-              </MUI.ListItemSecondaryAction>
+              </ListItemSecondaryAction>
             )}
-          </MUI.ListItem>
-
-          {/*<MUI.Collapse in={groupOpens[i]} timeout="auto" unmountOnExit>
-              <MUI.List component="div" disablePadding>
-                 {group.entries.map((entry, j) => (
-                      <EntryListItem
-                        key={j}
-                        entry={entry}
-                        index={i}
-                        selected={selectedIndex === i}
-                        className={classes.nested}
-                      />
-                    ))}
-              </MUI.List>
-            </MUI.Collapse>*/}
+          </ListItem>
         </React.Fragment>
       ))}
-    </MUI.List>
+    </List>
   )
 }
 
