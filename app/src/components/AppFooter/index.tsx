b59996a75052d37b9a022779ff077f9ef452de1f
Keiya Watanabe <ohagi2149@gmail.com>
2021-10-02 23:31:14 +0900

    MUIまとめないように修正

--- app/src/components/AppFooter/index.tsx
+++ app/src/components/AppFooter/index.tsx
@@ -1,4 +1,4 @@
-import * as MUI from '@material-ui/core'
+import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core'
 import INfoRoundedIcon from '@material-ui/icons/InfoRounded'
 import { makeStyles } from '@material-ui/core/styles'
 
@@ -8,28 +8,29 @@ const useStyles = makeStyles((theme) => ({
     bottom: 0,
     zIndex: theme.zIndex.speedDial - 1,
   },
-  text: { color: 'gray' },
+    text: { color: 'gray' },
 }))
 
 const AppFooter = () => {
   const classes = useStyles()
 
   return (
-    <MUI.AppBar
+    <AppBar
       position="fixed"
       color="inherit"
       className={classes.appBar}
       elevation={0}
     >
-      <MUI.Toolbar variant="dense">
-        <MUI.IconButton edge="start">
+      <Toolbar variant="dense">
+        <IconButton edge="start">
           <INfoRoundedIcon />
-        </MUI.IconButton>
-        <MUI.Typography className={classes.text}>
+        </IconButton>
+
+        <Typography className={classes.text}>
           This is Sample Application.
-        </MUI.Typography>
-      </MUI.Toolbar>
-    </MUI.AppBar>
+        </Typography>
+      </Toolbar>
+    </AppBar>
   )
 }
 export default AppFooter
