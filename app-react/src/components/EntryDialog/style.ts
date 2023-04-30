08e8ef6432dc9efff8277aeea2e5c42ee1e458c5
Keiya Watanabe <ohagi2149@gmail.com>
2023-04-30 23:45:22 +0900

    refactor: 改行追加

--- app-react/src/components/EntryDialog/style.ts
+++ app-react/src/components/EntryDialog/style.ts
@@ -1,21 +1,43 @@
 import { makeStyles } from '@material-ui/core/styles'
 
 export const useStyles = makeStyles((theme) => ({
-  dialogTitle: { paddingBottom: '0' },
-  half: { margin: ' 0 1rem 1rem 0', width: '95%' },
-  textField: { marginBottom: '1rem', width: '100%' },
-  password: { width: '100%' },
-  generator: { marginBottom: '1rem' },
-  formControl: { margin: theme.spacing(1), minWidth: 120 },
+  dialogTitle: {
+    paddingBottom: '0',
+  },
+  half: {
+    margin: ' 0 1rem 1rem 0',
+    width: '95%',
+  },
+  textField: {
+    marginBottom: '1rem',
+    width: '100%',
+  },
+  password: {
+    width: '100%',
+  },
+  generator: {
+    marginBottom: '1rem',
+  },
+  formControl: {
+    marginTop: theme.spacing(1),
+    minWidth: 200,
+  },
   date: {
     fontSize: '12px',
     color: 'gray',
     textAlign: 'right',
     marginBottom: '0.5rem',
   },
-  expired: { textAlign: 'right' },
-  warning: { verticalAlign: 'middle' },
-  warningIcon: { verticalAlign: 'middle', margin: '0 5px 3px 0' },
+  expired: {
+    textAlign: 'right',
+  },
+  warning: {
+    verticalAlign: 'middle',
+  },
+  warningIcon: {
+    verticalAlign: 'middle',
+    margin: '0 5px 3px 0',
+  },
   passwordLevel_high: {
     '& > .MuiLinearProgress-bar': {
       background: '#ff0000',
